9a1d7d3dc24a6d6e8c733a821805ad9f58686801
Lucas Russo <lerwys@gmail.com>
2023-03-29 18:25:39 -0700

    platform/xilinx/7series: add missing ODELAYE2 sim primitive

--- gateware/platform/xilinx/7series/ODELAYE2.v
+++ gateware/platform/xilinx/7series/ODELAYE2.v
@@ -1,31 +1,28 @@
-// This is super basic. Not anywhere near to complete!!!
+module ODELAYE2 (
+    output [4:0] CNTVALUEOUT,
+    output reg DATAOUT,
+    input C,
+    input CE,
+    input CINVCTRL,
+    input CLKIN,
+    input [4:0] CNTVALUEIN,
+    input INC,
+    input LD,
+    input LDPIPEEN,
+    input ODATAIN,
+    input REGRST
+);
 
-`timescale 1 ns / 1 ps
-
-module ODELAYE2 (CNTVALUEOUT, DATAOUT, C, CE, CINVCTRL, CLKIN, CNTVALUEIN, INC, LD, LDPIPEEN, ODATAIN, REGRST);
-    parameter CINVCTRL_SEL = "FALSE";
-    parameter DELAY_SRC = "ODATAIN";
-    parameter HIGH_PERFORMANCE_MODE    = "FALSE";
-    parameter [0:0] IS_C_INVERTED = 1'b0;
-    parameter [0:0] IS_ODATAIN_INVERTED = 1'b0;
-    parameter ODELAY_TYPE  = "FIXED";
-    parameter integer ODELAY_VALUE = 0;
-    parameter PIPE_SEL = "FALSE";
-    parameter real REFCLK_FREQUENCY = 200.0;
-    parameter SIGNAL_PATTERN    = "DATA";
-
-    output [4:0] CNTVALUEOUT;
-    output reg DATAOUT;
-    input C;
-    input CE;
-    input CINVCTRL;
-    input CLKIN;
-    input [4:0] CNTVALUEIN;
-    input INC;
-    input LD;
-    input LDPIPEEN;
-    input ODATAIN;
-    input REGRST;
+parameter CINVCTRL_SEL = "FALSE";
+parameter DELAY_SRC = "ODATAIN";
+parameter HIGH_PERFORMANCE_MODE    = "FALSE";
+parameter [0:0] IS_C_INVERTED = 1'b0;
+parameter [0:0] IS_ODATAIN_INVERTED = 1'b0;
+parameter ODELAY_TYPE  = "FIXED";
+parameter integer ODELAY_VALUE = 0;
+parameter PIPE_SEL = "FALSE";
+parameter real REFCLK_FREQUENCY = 200.0;
+parameter SIGNAL_PATTERN    = "DATA";
 
 // To get a bit more than 360 degree range
 localparam real TAP_DEL = (1.0 / REFCLK_FREQUENCY * 1000 / 31);
