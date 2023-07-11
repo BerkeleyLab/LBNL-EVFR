4c2e0e249a997c1db9f2ea6fe549904d6b51b3f7
Keith Penney <kpenney@lbl.gov>
2023-07-10 17:10:13 -0700

    Modified I2C command decoder

--- gateware/modules/iicCommandTable_decoder.py
+++ gateware/modules/iicCommandTable_decoder.py
@@ -2,10 +2,13 @@
 This script decodify the iicCommandTable.dat used by Marble i2c_chunk.v (i2cBridge) printing a report
 """
 
+import argparse
 from os.path import isfile
 from datetime import datetime
 
 
+# Bus select is done by writing bitmask to U5 (TCA9548) (I2C address 0xE0)
+BUSMUX_ADDRESS = 0xe0
 TCA9548_bus = {
     0: "FMC1",
     1: "FMC2",
@@ -16,64 +19,117 @@ TCA9548_bus = {
     6: "APP",
 }
 
+def _selected(bitmask=0):
+    """Return string of selected buses in bitmask"""
+    l = []
+    for ch, name in TCA9548_bus.items():
+        if (1<<ch) & bitmask:
+            l.append(name)
+    return ', '.join(l)
 
-def main(file_path="./iicCommandTable.dat"):
+def main(file_path=None):
+    if file_path is None:
+        file_path="./iicCommandTable.dat"
     cmd_table = load_file(file_path)
+    pa = 0
     if not cmd_table: return
     else:
         report_file = f"File {cmd_table.name} - {datetime.now()}\n---- Start of report ----\n"
-        report_file += "[instruction] -> description and data\n"
+        report_file += "Prog Address : Cmd Byte : [op_code n_code] -> Description and data\n"
     for idx, cmd_line in enumerate(cmd_table):
-        op_code = int(cmd_line[:2], 16) >> 5
-        n_code = int(cmd_line[:2], 16) & 0x1f
+        cmd_byte = int(cmd_line[:2], 16)
+        op_code = cmd_byte >> 5
+        n_code = cmd_byte & 0x1f
 
+        report_file += f"{pa:03x}: {cmd_byte:02x} : [{op_code:03b} {n_code:05b}] -> "
         match op_code:
             case 0:  # special
                 if n_code == 0:
-                    report_file += f"[{hex(int(cmd_line[:2], 16))}] -> sleep\n"
+                    #report_file += f"[{hex(int(cmd_line[:2], 16))}] -> sleep\n"
+                    report_file += f"sleep\n"
                 elif n_code == 2:
-                    report_file += f"[{hex(int(cmd_line[:2], 16))}] -> result buffer flip\n"
+                    report_file += f"result buffer flip\n"
                 elif n_code == 3:
-                    report_file += f"[{hex(int(cmd_line[:2], 16))}] -> trigger logic analyzer\n"
+                    report_file += f"trigger logic analyzer\n"
                 elif n_code >= 16:
-                    report_file += f"[{hex(int(cmd_line[:2], 16))}] -> select hardware bus {n_code-16}\
-                                                                            - {TCA9548_bus[n_code-16]}\n"
+                    cfg = n_code & 0xf
+                    report_file += f"hw_config 0b{cfg:04b}\n"
 
             case 1:  # read
-                report_file += f"[{hex(int(cmd_line[:2], 16))}] -> read - addr: \
-                                  {hex(int(next(cmd_table)[:2], 16))} - data number: {n_code-1}\n"
+                dlen = n_code-1
+                devaddr = int(next(cmd_table)[:2], 16)
+                pa += 1
+                report_file += f"read  - dev_addr: 0x{devaddr:02x} - data number: {dlen}\n"
 
             case 2:  # write
-                report_file += f"[{hex(int(cmd_line[:2], 16))}] -> write - addr: \
-                                  {hex(int(next(cmd_table)[:2], 16))} - data:"
-                for j in range(n_code-1): report_file += f" {hex(int(next(cmd_table)[:2], 16))}"
+                #n_code = 1 + addr_bytes + len(data)
+                #nbytes=n_code-1
+                devaddr = int(next(cmd_table)[:2], 16)
+                pa += 1
+                # Catch the special case of a bus mux write
+                if devaddr == BUSMUX_ADDRESS:
+                    bitmask = int(next(cmd_table)[:2], 16)
+                    seltext = _selected(bitmask)
+                    pa += 1
+                    report_file += f"Busmux - bitmask: 0b{bitmask:08b}" + \
+                                    f" Selected {seltext}"
+                else:
+                    report_file += f"write - dev_addr:" + \
+                                    f" 0x{devaddr:02x} - mem_addr+data:"
+                    for j in range(n_code-1):
+                        data = int(next(cmd_table)[:2], 16)
+                        pa += 1
+                        report_file += f" 0x{data:02x}"
                 report_file += '\n'
 
             case 3:  # write followed by repeated start
-                report_file += f"[{hex(int(cmd_line[:2], 16))}] -> write - addr: \
-                                  {hex(int(next(cmd_table)[:2], 16))} - START - data:"
-                for j in range(n_code-2): report_file += f" {hex(int(next(cmd_table)[:2], 16))}"
+                addr_bytes = n_code-1
+                devaddr = int(next(cmd_table)[:2], 16)
+                memaddr = int(next(cmd_table)[:2], 16)
+                pa += 2
+                if addr_bytes == 2:
+                    memaddr = (memaddr << 8) + int(next(cmd_table)[:2], 16)
+                    pa += 1
+                report_file += f"write - dev_addr:" + \
+                                f" 0x{devaddr:02x} - mem_addr: 0x{memaddr:04x} - START - data:"
+                for j in range(n_code-2):
+                    data = int(next(cmd_table)[:2], 16)
+                    pa += 1
+                    report_file += f" 0x{data:02x}"
                 report_file += '\n'
 
             case 4:  # pause (ticks are 8 bit times)
-                report_file += f"[{hex(int(cmd_line[:2], 16))}] -> short pause of {n_code} cycles\n"
+                report_file += f"short pause of {n_code} cycles\n"
 
             case 5:  # pause (ticks are 256 bit times)
-                report_file += f"[{hex(int(cmd_line[:2], 16))}] -> long pause of {n_code*32} cycles\n"
+                report_file += f"long pause of {n_code*32} cycles\n"
 
             case 6:  # jump
-                report_file += f"[{hex(int(cmd_line[:2], 16))}] -> jump +{n_code} ({hex(n_code)}) lines\n"
+                jump_addr = n_code*32
+                report_file += f"jump +{n_code} to address 0x{jump_addr:03x}" + \
+                                f" ({hex(n_code)}) lines\n"
 
             case 7:  # set result address
-                report_file += f"[{hex(int(cmd_line[:2], 16))}] -> set result address {n_code} ({hex(n_code)})\n"
+                result_address = 0x800 + n_code*32
+                report_file += f"set result address {n_code} to" + \
+                                f" address 0x{result_address:03x} ({hex(n_code)})\n"
+        pa += 1
+
     report_file += "---- End of report ----\n"
     print(report_file)
+    return
 
 
 def load_file(file_path="./iicCommandTable.dat"):
+    binary = True
     if isfile(file_path):
+        if is_plaintext(filename):
+            binary = False
         try:
-            return iter(open(file_path, 'rb'))
+            if binary:
+                return iter(open(file_path, 'rb'))
+            else:
+                return iter(open(file_path, 'r'))
         except Exception as e:
             print(f"Error during file opening: [{e}]")
             return None
@@ -81,5 +137,24 @@ def load_file(file_path="./iicCommandTable.dat"):
         print(f'File "{file_path}" not found')
         return None
 
+def is_plaintext(file):
+    """Returns True if file with filename 'file' is plaintext (ASCII/utf-8), False otherwise."""
+    _ascii = True
+    with open(file, 'r') as fd:
+        line = True
+        while line:
+            try:
+                line = fd.read(100)
+            except UnicodeDecodeError as e:
+                _ascii = False
+                break
+    return _ascii
 
-if __name__ == '__main__': main()
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+    else:
+        filename = None
+    main(filename)
