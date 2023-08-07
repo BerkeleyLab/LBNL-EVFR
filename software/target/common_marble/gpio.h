107671cb80d4f07f1f86a03b68c9ec694cfc3e37
Alessio Amodio <aamodio@lbl.gov>
2023-08-07 16:40:51 -0700

    gpio.h: fanout MGT indexes added

--- software/target/common_marble/gpio.h
+++ software/target/common_marble/gpio.h
@@ -20,7 +20,7 @@
 #define GPIO_IDX_I2C_CHUNK_CSR            6 // IIC state machine control/status
 #define GPIO_IDX_IIC_PROC                 7 // On-board IIC components
 #define GPIO_IDX_FAN_TACHOMETERS          8 // Monitor internal fans
-#define GPIO_IDX_EVR_MGT_DRP_CSR          9 // MGT control/status
+#define GPIO_IDX_EVR_MGT_DRP_CSR          9 // EVR MGT control/status
 #define GPIO_IDX_EVR_MGT_STATUS          10 // Additional MGT status (R)
 #define GPIO_IDX_EVR_MONITOR_CSR         11 // EVR control/status
 #define GPIO_IDX_EVR_MONITOR_SECONDS     12 // EVR monitor seconds (POSIX)
@@ -38,6 +38,9 @@
 #define GPIO_IDX_CONFIG_KD_GATE_DRIVER   28 // Configure kicker driver outputs
 #define GPIO_IDX_EVR_SELECT_OUTPUT       29 // Choose EVR output
 #define GPIO_IDX_EVR_CONFIG_OUTPUT       30 // Configure EVR output
+#define GPIO_IDX_MGT_HW_CONFIG           31 // MGT hardware configuration
+#define GPIO_IDX_EVF_MGT_DRP_CSR         32 // EVF MGT control/status
+#define GPIO_IDX_PER_MGTWRAPPER           1 // EVF MGT register offset
 
 #include <xil_io.h>
 #include <xparameters.h>
